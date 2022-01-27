f9def246e192a017044c5b1be0364b699db77999
andym388 <andym388@hotmail.com>
2022-01-27 16:07:43 +1100

    Change#2 Author Routes

--- routes/authors.js
+++ routes/authors.js
@@ -24,26 +24,6 @@ router.get('/new', (req, res) => {
     res.render('authors/new', { author: new Author() })
   })
 
-/*
-// Create Author Route
-router.post('/', (req, res) => {
-    const author = new Author({
-        name: req.body.name
-      })
-    author.save((err, newAuthor) => {
-    if (err) {
-      res.render('authors/new', {
-        author: author,
-        errorMessage: 'Error creating Author'
-      })
-    } else {
-        // res.redirect(`authors/${newAuthor.id}`)
-        res.redirect(`authors`)
-    }
-  })
-})
-*/
-
 // Create Author Route
 router.post('/', async (req, res) => {
     const author = new Author({
